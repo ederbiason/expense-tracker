1b7ca23bc04a23eecb95c329fb9b6acd3a90dc29
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-01 16:26:33 -0300

    data and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import * as C from './App.styles'
+import { useState } from 'react';
+import { Item } from './types/Item'
+import { Category } from './types/Category'
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth } from './helpers/dateFilter';
 
 export function App() {
+  const [list, setList] = useState(items)
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+
   return (
     <C.Container>
       <C.Header>
---
+++ src/data/items.ts
@@ -0,0 +1,31 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    {
+        date: new Date(),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12
+    },
+
+    {
+        date: new Date(),
+        category: 'food',
+        title: 'Burguer King',
+        value: 28
+    },
+
+    {
+        date: new Date(),
+        category: 'rent',
+        title: 'Apt rent',
+        value: 2300
+    },
+
+    {
+        date: new Date(),
+        category: 'salary',
+        title: 'Salary AVENUECODE',
+        value: 4500
+    }
+]
