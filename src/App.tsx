2ba0fa1530461d3feec7251e26e86257352955ef
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-01 16:04:39 -0300

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import * as C from './App.styles'
 
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Finance System</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        
+      </C.Body>
+    </C.Container>
   );
 }
