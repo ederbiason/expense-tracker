2665874d88dacfa3fbf5aef7ebe6e174251cb5f3
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-06-02 15:31:37 -0300

    table item exhibition

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,6 +1,7 @@
 import { formatDate } from '../../helpers/dateFilter';
 import { Item } from '../../types/Item';
-import * as C from './styles'
+import * as C from './styles';
+import { categories } from '../../data/categories';
 
 type Props = {
     item: Item;
@@ -10,9 +11,17 @@ export function TableItem({ item }: Props) {
     return (
         <C.TableLine>
             <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
-            <C.TableColumn>{item.category}</C.TableColumn>
+            <C.TableColumn>
+                <C.Category color={categories[item.category].color}>
+                    {categories[item.category].title}
+                </C.Category>
+            </C.TableColumn>
             <C.TableColumn>{item.title}</C.TableColumn>
-            <C.TableColumn>$ {item.value}</C.TableColumn>
+            <C.TableColumn>
+                <C.Value color={categories[item.category].expense ? 'red' : 'green'}>
+                    $ {item.value}
+                </C.Value>
+            </C.TableColumn>
         </C.TableLine>
     )
 }
